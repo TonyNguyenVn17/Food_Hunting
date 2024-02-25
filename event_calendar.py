cc1c4042983ee2ce7c84dde2ce4f4d513d9ecd32
Huy Bui <huyvijamco@gmail.com>
2024-02-24 23:01:11 -0500

    Automatically check if log in or not. If not re-opening the sign in page until user has logged in. If logged in then continue like usual

--- event_calendar.py
+++ event_calendar.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from time import sleep
 from dotenv import load_dotenv
 import os
@@ -8,21 +9,31 @@ import os
 load_dotenv()
 CHROME_PROFILE_PATH = os.getenv('CHROME_PROFILE_PATH')
 # CHROME_PROFILE_PATH = '/home/<username>/.config/google-chrome/Default'
-
-
 def eventCalendar():
     options = webdriver.ChromeOptions()
     # Path to your chrome profile (Either Linux/Mac/Windows)
     # How to find chrome profile https://www.howtogeek.com/255653/how-to-find-your-chrome-profile-folder-on-windows-mac-and-linux/
     options.add_argument(f'--user-data-dir={CHROME_PROFILE_PATH}')
     driver = webdriver.Chrome(options)
+
+    def check_exists_by_ID(ID):
+        try:
+            driver.find_element(By.ID, ID)
+        except:
+            return False
+        return True
+
     # Auto login
     driver.get('https://www.campusgroups.com/shibboleth/login?idp=usf')
     sleep(10)
-    # Opening event calendar with tag food
-    driver.get('https://bullsconnect.usf.edu/events?topic_tags=7276307')
-    sleep(10)
-
+    # Loop until a hooman log in and authenticate
+    while check_exists_by_ID("i0281")==True: # A random element in the USF login website
+        print('Error: Need hooman authentication')
+        driver.get('https://www.campusgroups.com/shibboleth/login?idp=usf')
+        sleep(30) # 30 seconds to enter username, password and authenticate
+    else:
+        driver.get('https://bullsconnect.usf.edu/events?topic_tags=7276307')
+        sleep(10)
 
 if __name__ == "__main__":
     eventCalendar()
