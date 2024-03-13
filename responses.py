fb528efbf6bf00a616f588317feb157f02966293
Jack Nguyen <nguyenminhtam7124@gmail.com>
2024-03-13 03:22:55 -0400

    Using SQLite to allow Discord Async

--- responses.py
+++ responses.py
@@ -4,7 +4,7 @@ from typing import List, Dict
 from db import FoodDatabase
 
 
-def get_response(user_input : str) -> str:
+def get_response(user_input: str) -> str:
     lowered: str = user_input.lower()
     if lowered[0] != "!":
         return None
@@ -26,42 +26,32 @@ def get_response(user_input : str) -> str:
         # data = scrape_data() return data directly from bullsconnect
         # return event from database
         db = FoodDatabase()
+        # TODO: Replace with today's event
         data = db.get_all_event()
         output = ""
+
+        if not data:
+            return "No events found in database, check back after 12AM or 12PM"
+
         for index, event in enumerate(data):
             event_str = f"## Event {index+1} \n > **Name**: {event[0]}\n > **Location**: {event[4]}\n > **Time**: {event[3]}\n"
             output += str(event_str)
-            
-        
-        # for name,event_list in data.items():
-        #     for event in event_list:
-        #         events.append(event)
-        #         names.append(name)
-        # formatted_events = []
-        # for i in range(len(events[:10])):
-        #     one_event = f"Event {i+1}: {names[i]} \nLocation: {events[i]['location']} \nTime: {events[i]['time']}\n"
-        #     formatted_events.append(one_event)
+
         return output
     elif lowered.startswith('help'):
-        return """!hello: Say hello to users \n good?: My feeling now \n !bye: Say goodbye to Bot \n !dice: How lucky are you today \n !events: List of food events today \n !help: Showing this lists"""
+        return """!hello: Say hello to users \n !good: My feeling now \n !bye: Say goodbye to Bot \n !dice: How lucky are you today \n !events: List of food events today \n !help: Showing this lists"""
     else:
         return choice(['I do not understand...',
                        'What are you talking about?',
                        'Do you mind rephrasing that?'])
-    
-
-#def generate_data(name, time, location, URL):
-    ##   'name': name,
-     #   'time': time,
-     #   'location': location,
-     #   'URL': URL
-   # }
 
 
 def scrape_data() -> Dict[str, List[Dict[str, str]]]:
+
+    # TODO: Implement this function
     """
-    run at 12AM daily
-    add data to db
+    scrape at 12AM and 12PM daily
+    then add data to db
     """
     db = FoodDatabase()
     check_login()
@@ -78,10 +68,9 @@ def scrape_data() -> Dict[str, List[Dict[str, str]]]:
             "location": event['location']
         }]
         db.add_event(event)
-         
+
 
 if __name__ == "__main__":
-    cmd='!events'
-    output=get_response(cmd)
+    cmd = '!events'
+    output = get_response(cmd)
     print(output)
-    
